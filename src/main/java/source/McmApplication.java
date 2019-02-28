0f990f1b8f5c1c7b650180d84fb3b16f30c40bcc
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-02-28 15:44:10 +0900

    webアプリのための雛形作成

--- src/main/java/source/McmApplication.java
+++ src/main/java/source/McmApplication.java
@@ -1,11 +1,9 @@
-package com.mcm;
+package source;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class McmApplication {
     public static void main(String[] args) {
         SpringApplication.run(McmApplication.class, args);
