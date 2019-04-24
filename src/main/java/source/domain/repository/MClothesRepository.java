e9e0b07b2a46316480409f58a5e8c8747eede27f
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-04-24 10:24:05 +0900

    JPARepositoryにおいて、Specificationを追加

--- src/main/java/source/domain/repository/MClothesRepository.java
+++ src/main/java/source/domain/repository/MClothesRepository.java
@@ -1,7 +1,8 @@
 package source.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import source.domain.entity.MClothes;
 
-public interface MClothesRepository extends JpaRepository<MClothes, Long> {
+public interface MClothesRepository extends JpaRepository<MClothes, Long>, JpaSpecificationExecutor {
 }
---
+++ src/main/java/source/domain/repository/specification/MClothesSpecification.java
@@ -0,0 +1,52 @@
+package source.domain.repository.specification;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
+import source.domain.entity.MClothes;
+
+import java.util.Date;
+
+public class MClothesSpecification {
+
+    public static Specification<MClothes> brandIdContains(final Integer brandId) {
+        return brandId == null ? null : (root, query, cb) -> {
+            return cb.equal(root.get("brandId"), brandId);
+        };
+    }
+
+    public static Specification<MClothes> genreIdEqual(final Integer genreId) {
+        return genreId == null ? null : (root, query, cb) -> {
+            return cb.equal(root.get("genreId"), genreId);
+        };
+    }
+
+    public static Specification<MClothes> shopIdEqual(final Integer shopId) {
+        return shopId == null ? null : (root, query, cb) -> {
+            return cb.equal(root.get("shopId"), shopId);
+        };
+    }
+
+    public static Specification<MClothes> priceLessEqual(final Integer price) {
+        return price == null ? null : (root, query, cb) -> {
+            return cb.lessThanOrEqualTo(root.get("price"), price);
+        };
+    }
+
+    public static Specification<MClothes> priceGreaterEqual(final Integer price) {
+        return price == null ? null : (root, query, cb) -> {
+            return cb.greaterThanOrEqualTo(root.get("price"), price);
+        };
+    }
+
+    public static Specification<MClothes> buyDateEqual(final Date buyDate) {
+        return buyDate == null ? null : (root, query, cb) -> {
+            return cb.equal(root.get("buyDate"), buyDate);
+        };
+    }
+
+    public static Specification<MClothes> deleteFlagEqual(final boolean deleteFlag) {
+        return (root, query, cb) -> {
+            return cb.equal(root.get("deleteFlag"), deleteFlag);
+        };
+    }
+}
