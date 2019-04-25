8d166459d10178af07c78307efe5e60a0d90d095
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-04-25 11:04:56 +0900

    検索ユースケースでのアウトプットドメインに変換する処理を追加

--- src/main/java/source/domain/entity/list/MClothesList.java
+++ src/main/java/source/domain/entity/list/MClothesList.java
@@ -1,4 +1,30 @@
 package source.domain.entity.list;
 
+import lombok.Value;
+import source.domain.dto.clothes.ClothesSearchOutputData;
+import source.domain.entity.MClothes;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Value(staticConstructor = "of")
 public class MClothesList {
+
+    private List<MClothes> values;
+
+    public List<ClothesSearchOutputData> convertSearchOutputDatas() {
+        return this.values.stream().map(value -> {
+
+            return ClothesSearchOutputData.builder()
+                    .imagePath(value.getMImage().getPath())
+                    .genreName(value.getMGenre().getName())
+                    .brandName(value.getMBrand().getName())
+                    .shopName(value.getMShop().getName())
+                    .price(value.getPrice())
+                    .buyDate(value.getBuyDate())
+                    .deleteFlag(value.isDeleteFlag())
+                    .build();
+
+        }).collect(Collectors.toList());
+    }
 }
