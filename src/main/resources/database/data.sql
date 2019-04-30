3cb8dab0d23f9a80a73bf00a6728ae1a217681b6
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-04-30 09:13:25 +0900

    ジャンルマスタの初期値を設定

---
+++ src/main/resources/database/data.sql
@@ -0,0 +1,11 @@
+-- ジャンルマスタの初期値を設定
+insert into m_genre (genre_name, delete_flag) values
+('Tops', false),
+('Tee', false),
+('Outer', false),
+('Bottoms', false),
+('Hat', false),
+('Bag', false),
+('Accessories', false),
+('Shoes & Boots', false),
+('All Items', false);
