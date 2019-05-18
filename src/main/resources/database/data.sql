e2f29b57bf3361923b5f96020ab705a11207ffac
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-05-18 18:11:08 +0900

    テストデータを作成

--- src/main/resources/database/data.sql
+++ src/main/resources/database/data.sql
@@ -1,11 +1,28 @@
 -- ジャンルマスタの初期値を設定
-insert into m_genre (genre_name, delete_flag) values
-('Tops', false),
-('Tee', false),
-('Outer', false),
-('Bottoms', false),
-('Hat', false),
-('Bag', false),
-('Accessories', false),
-('Shoes & Boots', false),
-('All Items', false);
+insert into m_genre (name)
+values
+    ('Tops'),
+    ('Tee'),
+    ('Outer'),
+    ('Bottoms'),
+    ('Hat'),
+    ('Bag'),
+    ('Accessories'),
+    ('Shoes & Boots'),
+    ('All Items');
+
+insert into m_image (name, path)
+values
+    ('bukht.jpg', '@/images/brand/icon/bukht_icon.jpg');
+
+insert into m_brand (name, link, image_id, country)
+values
+    ('bukht', 'http://bukht.com/', 1, '日本');
+
+insert into m_shop (name, link, station_name, image_id, address, business_hours, tel)
+values
+    ('O代官山 本店', 'http://store.moc-o.com/', '代官山', 1, '東京都渋谷区猿楽町26-13 グレイス代官山 #202', '12:00 - 20:00 定休日 無し', '03-6416-1187');
+
+insert into m_clothes (image_id, genre_id, brand_id, shop_id, price, buy_date)
+values
+    (1, 1, 1, 1, 10000, '2019/01/01');
