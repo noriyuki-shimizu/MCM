bf709f1d989402d900dfe2c0ee016cb2dc9a139d
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-06-02 22:36:44 +0900

    共通カラムに関するスーパークラスを追加

---
+++ src/main/java/source/domain/entity/common/TimestampEntity.java
@@ -0,0 +1,43 @@
+package source.domain.entity.common;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+import java.util.Date;
+
+@MappedSuperclass
+public abstract class TimestampEntity {
+
+    @Column(name = "update_date_time")
+    public Timestamp updateDateTime;
+
+    @Column(name = "create_date_time", updatable=false)
+    public Timestamp createDateTime;
+
+    @Column(name = "delete_date_time", updatable=false)
+    public Timestamp deleteDateTime;
+
+    @Column(name = "delete_flag")
+    @NotNull
+    private boolean deleteFlag;
+
+    @PrePersist
+    public void prePersist() {
+        Timestamp ts = new Timestamp((new Date()).getTime());
+        this.createDateTime = ts;
+        this.updateDateTime = ts;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        Timestamp ts = new Timestamp((new Date()).getTime());
+        this.updateDateTime = ts;
+
+        if (this.deleteFlag) {
+            this.deleteDateTime = ts;
+        }
+    }
+}
