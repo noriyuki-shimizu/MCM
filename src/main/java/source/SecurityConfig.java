11639203bc92f7fcd36225543af56c0756a10a85
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-06-06 23:36:58 +0900

    認証周りの処理を削除

--- src/main/java/source/SecurityConfig.java
+++ src/main/java/source/SecurityConfig.java
@@ -1,29 +1,14 @@
 package source;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.GenericFilterBean;
 import source.domain.env.AuthEnv;
 import source.domain.repository.BUserRepository;
-import source.usecases.interactor.authentication.*;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -37,37 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity httpSecurity) {
         try {
-            httpSecurity
-                    .authorizeRequests()
-                        .mvcMatchers("/preLogin")
-                            .permitAll()
-                        .anyRequest()
-                            .authenticated()
-                    .and()
-                    .cors()
-                        .configurationSource(this.corsConfigurationSource())
-                    .and()
-                    .exceptionHandling()
-                        .authenticationEntryPoint(authenticationEntryPoint())
-                        .accessDeniedHandler(accessDeniedHandler())
-                    .and()
-                    // LOGIN
-                    .formLogin()
-                        .loginProcessingUrl("/login").permitAll()
-                        .usernameParameter("eMail")
-                        .passwordParameter("password")
-                        .successHandler(authenticationSuccessHandler())
-                        .failureHandler(authenticationFailureHandler())
-                    .and()
-                    .logout()
-                        .logoutUrl("/logout")
-                        .logoutSuccessHandler(logoutSuccessHandler())
-                    .and()
-                    .csrf()
-                        .disable()
-//                    .addFilterBefore(tokenFilter(), UsernamePasswordAuthenticationFilter.class)
-                    .sessionManagement()
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            httpSecurity.cors().configurationSource(this.corsConfigurationSource());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -89,47 +44,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
 
         return corsConfigurationSource;
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.eraseCredentials(true)
-                .userDetailsService(simpleUserDetailsService())
-                .passwordEncoder(passwordEncoder());
-    }
-
-    @Bean("simpleUserDetailsService")
-    UserDetailsService simpleUserDetailsService() {
-        return new SimpleUserDetailsService(this.userRepository);
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    GenericFilterBean tokenFilter() {
-        return new SimpleTokenFilter(userRepository, authEnv.getSecret());
-    }
-
-    AuthenticationEntryPoint authenticationEntryPoint() {
-        return new SimpleAuthenticationEntryPoint();
-    }
-
-    AccessDeniedHandler accessDeniedHandler() {
-        return new SimpleAccessDeniedHandler();
-    }
-
-    AuthenticationSuccessHandler authenticationSuccessHandler() {
-        return new SimpleAuthenticationSuccessHandler(authEnv.getSecret());
-    }
-
-    AuthenticationFailureHandler authenticationFailureHandler() {
-        return new SimpleAuthenticationFailureHandler();
-    }
-
-    LogoutSuccessHandler logoutSuccessHandler() {
-        return new HttpStatusReturningLogoutSuccessHandler();
     }
 
 }
