a2edda60b863db318b621d1e3d7617766ef69cd2
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-06-23 23:25:10 +0900

    認証させないための設定を追加

--- src/main/java/source/annotation/NonAuth.java
+++ src/main/java/source/annotation/NonAuth.java
@@ -1,4 +1,14 @@
 package source.annotation;
 
-public class NonAuth {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * non require auth
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface NonAuth {
 }
