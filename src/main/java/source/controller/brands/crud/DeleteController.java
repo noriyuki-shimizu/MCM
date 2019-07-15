b9799b34b288923644d980937700f743346e6c8e
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-07-15 16:21:35 +0900

    ブランド削除バックエンド処理の完成

--- src/main/java/source/controller/brands/crud/DeleteController.java
+++ src/main/java/source/controller/brands/crud/DeleteController.java
@@ -1,9 +1,35 @@
 package source.controller.brands.crud;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import source.controller.brands.BrandsController;
+import source.usecases.brands.IBrandDeleteUsecase;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
-public class DeleteController {
+public class DeleteController extends BrandsController {
+
+    @Autowired
+    private IBrandDeleteUsecase usecase;
+
+    @DeleteMapping(value = "/{id}")
+    public String deleteHandler(@PathVariable("id") Long id) {
+        try {
+            return super.MAPPER.writeValueAsString(this.usecase.delete(id));
+        } catch (JsonProcessingException jpe) {
+            jpe.printStackTrace();
+
+            return null;
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+
+            return null;
+        }
+    }
 }
