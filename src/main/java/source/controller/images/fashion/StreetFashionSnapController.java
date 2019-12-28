39944930ccad757b3aaffb9678514e29cd147011
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-12-29 00:04:16 +0900

    RequestMapping を継承できなかったためメソッドの上に定義するよう修正

--- src/main/java/source/controller/images/fashion/StreetFashionSnapController.java
+++ src/main/java/source/controller/images/fashion/StreetFashionSnapController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import source.controller.images.ImageController;
 import source.domain.vo.ImageAddresses;
@@ -14,13 +13,12 @@ import source.usecases.app.images.IGetImageAddressUsecase;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping(value = "/street-fashion-snap")
 public class StreetFashionSnapController extends ImageController {
     @Autowired
     private IGetImageAddressUsecase usecase;
 
-    @GetMapping("/")
-    public String getImages() {
+    @GetMapping("/street-fashion-snap/addresses")
+    public String addresses() {
         try {
             ImageAddresses imageAddresses = this.usecase.handle();
             return super.MAPPER.writeValueAsString(imageAddresses.getValues());
