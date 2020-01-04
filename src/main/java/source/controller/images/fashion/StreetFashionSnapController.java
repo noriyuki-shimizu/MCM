1412b3330caf208f36569ea0c2e497bec7f350c0
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-01-04 21:23:04 +0900

    JSON の変換処理を削除。簡単なリファクタリングを実施。

--- src/main/java/source/controller/images/fashion/StreetFashionSnapController.java
+++ src/main/java/source/controller/images/fashion/StreetFashionSnapController.java
@@ -1,31 +1,23 @@
 package source.controller.images.fashion;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import source.controller.images.ImageController;
-import source.domain.vo.ImageAddresses;
 import source.usecases.app.images.IGetImageAddressUsecase;
+import source.usecases.dto.response.images.ImageAddressResponseData;
 
 @RestController
-@RequiredArgsConstructor
 @Slf4j
 public class StreetFashionSnapController extends ImageController {
+    private static final String BASE_URL = "/street-fashion-snap";
+
     @Autowired
     private IGetImageAddressUsecase usecase;
 
-    @GetMapping("/street-fashion-snap/addresses")
-    public String addresses() {
-        try {
-            ImageAddresses imageAddresses = this.usecase.handle();
-            return super.MAPPER.writeValueAsString(imageAddresses.getValues());
-        } catch (JsonProcessingException jpe) {
-            log.error("JSON の変換エラー", jpe);
-
-            return null;
-        }
+    @GetMapping(BASE_URL + "/addresses")
+    public ImageAddressResponseData addresses() {
+        return ImageAddressResponseData.of(this.usecase.handle());
     }
 }
