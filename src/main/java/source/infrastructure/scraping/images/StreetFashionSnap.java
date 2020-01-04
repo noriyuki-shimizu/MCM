d2d03e5f775ba9f817508743909d99101d1dc043
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-01-04 21:27:23 +0900

    例外処理を Usecase に移動。100ページまでをランダムで選択し、画像を取得するようロジックを修正

--- src/main/java/source/infrastructure/scraping/images/StreetFashionSnap.java
+++ src/main/java/source/infrastructure/scraping/images/StreetFashionSnap.java
@@ -9,7 +9,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 @Component
 @ConfigurationProperties(prefix = "settings.scraping.images")
@@ -19,20 +24,23 @@ public class StreetFashionSnap implements IScrapingImage {
     private String streetFashionSnapUrl;
 
     @Override
-    public Elements getImageAddresses() {
-        try {
-            Document document = Jsoup
-                    .connect(URLDecoder.decode(this.streetFashionSnapUrl, "UTF-8"))
-                    .get();
-            Elements elements = document.select("div.the-photo a.permalink img");
+    public List<String> pickImageAddresses() throws UnsupportedEncodingException, IOException {
+        String accessURL = new StringBuilder()
+                .append(URLDecoder.decode(this.streetFashionSnapUrl, "UTF-8"))
+                .append(new Random().nextInt(100) + 1)
+                .toString();
 
-            if (!(elements == null || elements.isEmpty())) {
-                return elements;
-            }
-            return null;
-        } catch (IOException ioe) {
-            log.error("指定した URL にアクセスできませんでした。確認してください。", ioe);
-            return null;
+        Document document = Jsoup
+                .connect(accessURL)
+                .get();
+        Elements elements = document.select("div.the-photo a.permalink img");
+
+        if (!(elements == null || elements.isEmpty())) {
+            return elements
+                    .stream()
+                    .map(element -> element.attr("src"))
+                    .collect(Collectors.toList());
         }
+        return Collections.emptyList();
     }
 }
