7d55627b1e08c043f8dea055017647d54d32ebca
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-01-04 21:30:07 +0900

    例外などのログ処理を追加

--- src/main/java/source/usecases/app/images/interactor/GetImageAddressInteractor.java
+++ src/main/java/source/usecases/app/images/interactor/GetImageAddressInteractor.java
@@ -1,22 +1,38 @@
 package source.usecases.app.images.interactor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import source.domain.vo.ImageAddresses;
 import source.infrastructure.scraping.images.IScrapingImage;
 import source.usecases.app.images.IGetImageAddressUsecase;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 @Component
+@Slf4j
 public class GetImageAddressInteractor implements IGetImageAddressUsecase {
     @Autowired
     private IScrapingImage scrapingImage;
 
     @Override
-    public ImageAddresses handle() {
-        List<String> imageAddresses = this.scrapingImage.pickImageAddresses();
+    public List<String> handle() {
+        try {
+            ImageAddresses imageAddresses = ImageAddresses.of(
+                    this.scrapingImage.pickImageAddresses()
+            );
 
-        return ImageAddresses.of(imageAddresses).chooseRamdom();
+            return imageAddresses
+                    .chooseRamdom()
+                    .getValues();
+        } catch (UnsupportedEncodingException uee) {
+            log.error("URL のデコードに失敗しました。", uee);
+            return null;
+        } catch (IOException ioe) {
+            log.error("指定した URL にアクセスできませんでした。確認してください。", ioe);
+            return null;
+        }
     }
 }
