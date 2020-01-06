ecf8329c8088a7b4240cd8ee2fdf65b0bb5888e7
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-01-06 19:40:55 +0900

    brand の view model DTO を作成

---
+++ src/main/java/source/usecases/dto/response/brands/BrandResponseViewModel.java
@@ -0,0 +1,10 @@
+package source.usecases.dto.response.brands;
+
+import lombok.Value;
+
+import java.util.List;
+
+@Value(staticConstructor = "of")
+public class BrandResponseViewModel {
+    private List<BrandResponseModel> brands;
+}
