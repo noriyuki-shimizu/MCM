29d649b93314708cfcae2fc5abbe7c2524522ac9
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-01-06 19:37:58 +0900

    クラス名を変更

--- src/main/java/source/usecases/dto/response/brands/BrandResponseModel.java
+++ src/main/java/source/usecases/dto/response/brands/BrandResponseModel.java
@@ -3,20 +3,17 @@ package source.usecases.dto.response.brands;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.Value;
-import source.domain.entity.Images;
 
 @Value(staticConstructor = "of")
 @Data
 public class BrandResponseViewModel {
     private Long id;
 
-    private Long userId;
-
     private String name;
 
     private String link;
 
-    private Images image;
+    private String imageLink;
 
     private String country;
 
