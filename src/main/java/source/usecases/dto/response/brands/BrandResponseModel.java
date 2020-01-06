ca84638b2475d1ae8417a5075131e6bf128d4bbb
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-01-06 19:41:12 +0900

    クラス名を変更

--- src/main/java/source/usecases/dto/response/brands/BrandResponseModel.java
+++ src/main/java/source/usecases/dto/response/brands/BrandResponseModel.java
@@ -6,7 +6,7 @@ import lombok.Value;
 
 @Value(staticConstructor = "of")
 @Data
-public class BrandResponseViewModel {
+public class BrandResponseModel {
     private Long id;
 
     private String name;
