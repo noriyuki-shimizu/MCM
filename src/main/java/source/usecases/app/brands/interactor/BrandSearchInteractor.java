ae985fe8efac3cf0dd8c46322fb644520561a976
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-01-06 19:40:06 +0900

    interface 変更に伴う interactor の修正

--- src/main/java/source/usecases/app/brands/interactor/BrandSearchInteractor.java
+++ src/main/java/source/usecases/app/brands/interactor/BrandSearchInteractor.java
@@ -3,13 +3,14 @@ package source.usecases.app.brands.interactor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specifications;
 import org.springframework.stereotype.Component;
-import source.usecases.dto.request.brands.BrandSearchRequestData;
 import source.domain.entity.Brands;
 import source.domain.repository.db.BrandsRepository;
 import source.domain.repository.db.specification.BrandsSpecification;
 import source.usecases.app.brands.IBrandSearchUsecase;
+import source.usecases.dto.response.brands.BrandResponseModel;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class BrandSearchInteractor implements IBrandSearchUsecase {
@@ -18,13 +19,24 @@ public class BrandSearchInteractor implements IBrandSearchUsecase {
     private BrandsRepository repository;
 
     @Override
-    public List<Brands> search(Long userId, BrandSearchRequestData inputData) {
-        return this.repository.findAll(
+    public List<BrandResponseModel> search(Long userId) {
+        List<Brands> brands = this.repository.findAll(
                 Specifications
                         .where(BrandsSpecification.userIdEqual(userId))
-                        .and(BrandsSpecification.nameLike(inputData.getName()))
-                        .and(BrandsSpecification.countryLike(inputData.getCountry()))
-                        .and(BrandsSpecification.isDeletedEqual(inputData.getIsDeleted()))
         );
+
+        return brands.stream()
+                .map(brand -> BrandResponseModel.of(
+                        brand.getId(),
+                        brand.getName(),
+                        brand.getLink(),
+                        brand.getImage() != null
+                                ? brand.getImage().getPath()
+                                : null,
+                        brand.getCountry(),
+                        brand.isDeleted()
+                        )
+                )
+                .collect(Collectors.toList());
     }
 }
