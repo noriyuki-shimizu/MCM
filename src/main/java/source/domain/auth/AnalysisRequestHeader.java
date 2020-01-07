f97257287bea948b496e5c43e1442bc446b5d12e
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-01-07 17:33:02 +0900

    リクエスト時のヘッダーからユーザIDを取得するロジックを追加

--- src/main/java/source/domain/auth/AnalysisRequestHeader.java
+++ src/main/java/source/domain/auth/AnalysisRequestHeader.java
@@ -5,9 +5,21 @@ import lombok.Value;
 import javax.servlet.http.HttpServletRequest;
 
 @Value(staticConstructor = "of")
-public class RequestToken {
+public class AnalysisRequestHeader {
 
     private HttpServletRequest request;
+
+    /**
+     * ユーザ ID を取得します.
+     * return {Long} userId
+     */
+    public Long getUserId() {
+        String userId = this.request.getHeader("UserId");
+        if (userId == null) {
+            return null;
+        }
+        return Long.parseLong(userId);
+    }
 
     /**
      * リクエストヘッダからトークンを取得します.
