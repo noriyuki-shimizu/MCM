4a6d050fa8745c3e26865c82d43a9cff47cafc87
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-01-11 14:26:18 +0900

    条件式を足すため repository を拡張

--- src/main/java/source/domain/repository/db/GenresRepository.java
+++ src/main/java/source/domain/repository/db/GenresRepository.java
@@ -1,13 +1,14 @@
 package source.domain.repository.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import source.domain.entity.Genres;
 
 @Repository
-public interface GenresRepository extends JpaRepository<Genres, Integer> {
+public interface GenresRepository extends JpaRepository<Genres, Integer>, JpaSpecificationExecutor {
     @Query(value = "UPDATE genres SET is_deleted = true, delete_date_time = now() WHERE id = :id", nativeQuery = true)
     public Genres deleteById(@Param("id") Long id);
 }
