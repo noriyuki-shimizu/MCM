70cea4c002edb92b6e4a2a42742665fc66b299c2
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-01-17 23:53:16 +0900

    アノテーションが足りていなかったため追加

--- src/main/java/source/controller/shops/crud/ShopRestorationController.java
+++ src/main/java/source/controller/shops/crud/ShopRestorationController.java
@@ -1,17 +1,21 @@
 package source.controller.shops.crud;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
 import source.controller.shops.ShopsController;
 import source.usecases.app.shops.ShopRestorationUsecase;
 import source.usecases.dto.response.shops.ShopResponseViewModel;
 
+@RestController
+@RequiredArgsConstructor
 public class ShopRestorationController extends ShopsController {
     @Autowired
     private ShopRestorationUsecase usecase;
 
-    @PutMapping(value = "/{id}")
+    @PutMapping(value = "/{id}/restoration")
     public ShopResponseViewModel handle(@PathVariable("id") Long id) {
         return ShopResponseViewModel.of(
                 this.usecase.restoration(id)
--- src/main/java/source/usecases/app/shops/interactor/ShopRestorationInteractor.java
+++ src/main/java/source/usecases/app/shops/interactor/ShopRestorationInteractor.java
@@ -1,11 +1,18 @@
 package source.usecases.app.shops.interactor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import source.domain.entity.Shops;
 import source.domain.repository.db.ShopsRepository;
 import source.usecases.app.shops.ShopRestorationUsecase;
 import source.usecases.dto.response.shops.ShopResponseModel;
 
+import javax.transaction.Transactional;
+
+@Component
+@Transactional
+@Slf4j
 public class ShopRestorationInteractor implements ShopRestorationUsecase {
     @Autowired
     private ShopsRepository repository;
