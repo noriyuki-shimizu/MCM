f73b28f606df2e416548e7150c70e6ac7f2337bd
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-01-17 23:55:01 +0900

    不要なアノテーションを削除

--- src/main/java/source/usecases/dto/response/shops/ShopResponseViewModel.java
+++ src/main/java/source/usecases/dto/response/shops/ShopResponseViewModel.java
@@ -1,10 +1,8 @@
 package source.usecases.dto.response.shops;
 
-import lombok.Data;
 import lombok.Value;
 
 @Value(staticConstructor = "of")
-@Data
 public class ShopResponseViewModel {
     private ShopResponseModel shop;
 }
--- src/main/java/source/usecases/dto/response/shops/ShopResponseViewModels.java
+++ src/main/java/source/usecases/dto/response/shops/ShopResponseViewModels.java
@@ -1,12 +1,10 @@
 package source.usecases.dto.response.shops;
 
-import lombok.Data;
 import lombok.Value;
 
 import java.util.List;
 
 @Value(staticConstructor = "of")
-@Data
 public class ShopResponseViewModels {
     private List<ShopResponseModel> shops;
 }
