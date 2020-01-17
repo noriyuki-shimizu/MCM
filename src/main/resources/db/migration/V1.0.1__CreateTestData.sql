0b19d99c1fec17b071ec17a7242414bb4c95580e
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-01-17 23:55:16 +0900

    SQL を修正

--- src/main/resources/db/migration/V1.0.1__CreateTestData.sql
+++ src/main/resources/db/migration/V1.0.1__CreateTestData.sql
@@ -19,7 +19,7 @@ values
 
 insert into shops (user_id, name, link, station_name, address, business_hours, tel)
 values
-    (1, 'O代官山 本店', 'http://store.moc-o.com/', '代官山', '東京都渋谷区猿楽町26-13 グレイス代官山 #202', '12:00 - 20:00 定休日 無し', '03-6416-1187');
+    (1, 'O代官山 本店', 'http://store.moc-o.com/', '代官山', '東京都渋谷区猿楽町26-13 グレイス代官山 #202', '12:00~20:00', '03-6416-1187');
 
 insert into clothes (user_id, genre_id, brand_id, shop_id, price, buy_date)
 values
