f5266a865441245ab07e588a399492da3d430e21
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-01-26 21:27:38 +0900

    関連テーブルの情報を取得し、そのエンティティをセットして更新するという処理に修正（TODO あり）

--- src/main/java/source/usecases/app/clothes/interactor/ClothesCreateInteractor.java
+++ src/main/java/source/usecases/app/clothes/interactor/ClothesCreateInteractor.java
@@ -3,7 +3,10 @@ package source.usecases.app.clothes.interactor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import source.domain.entity.*;
+import source.domain.repository.db.BrandsRepository;
 import source.domain.repository.db.ClothesRepository;
+import source.domain.repository.db.GenresRepository;
+import source.domain.repository.db.ShopsRepository;
 import source.presenter.clothes.IClothesMappingPresenter;
 import source.usecases.app.clothes.IClothesCreateUsecase;
 import source.usecases.app.images.IImageSaveUsecase;
@@ -13,7 +16,6 @@ import source.usecases.dto.response.clothes.ClothesResponseViewModel;
 
 import javax.transaction.Transactional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Component
 @Transactional
@@ -26,23 +28,36 @@ public class ClothesCreateInteractor implements IClothesCreateUsecase {
     private IImageSaveUsecase imageSaveUsecase;
 
     @Autowired
+    private BrandsRepository brandsRepository;
+
+    @Autowired
+    private ShopsRepository shopsRepository;
+
+    @Autowired
+    private GenresRepository genresRepository;
+
+    @Autowired
     private IClothesMappingPresenter presenter;
 
     @Override
     public ClothesResponseViewModel create(Long userId, ClothesCreateRequestModel inputData) {
         Images clothesImage = this.imageSaveUsecase.save(null, inputData.getImageLink());
 
-        Set<Genres> genres = inputData.getGenreIds()
-                .stream()
-                .map(genreId -> Genres.builder().id(genreId).build())
-                .collect(Collectors.toSet());
+        // TODO: JPA での闇の実装（もっと調査する必要あり）
+        Brands brand = this.brandsRepository.findOne(inputData.getBrandId());
+
+        Shops shop = this.shopsRepository.findOne(inputData.getShopId());
+
+        Set<Genres> genres = this.genresRepository.findByIdIn(
+                inputData.getGenreIds()
+        );
 
         Clothes clothes = Clothes.builder()
                 .userId(userId)
                 .image(clothesImage)
                 .genres(genres)
-                .brand(Brands.builder().id(inputData.getBrandId()).build())
-                .shop(Shops.builder().id(inputData.getShopId()).build())
+                .brand(brand)
+                .shop(shop)
                 .price(inputData.getPrice())
                 .buyDate(BuyDate.toSqlDate(inputData.getBuyDate()))
                 .comment(inputData.getComment())
