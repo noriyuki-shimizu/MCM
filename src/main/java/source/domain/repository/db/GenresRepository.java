65dd42cd6a64dfcee29324ab73b7b7b4cd4314f6
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-01-26 21:30:46 +0900

    id での in 句の検索処理を追加

--- src/main/java/source/domain/repository/db/GenresRepository.java
+++ src/main/java/source/domain/repository/db/GenresRepository.java
@@ -7,8 +7,12 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import source.domain.entity.Genres;
 
+import java.util.Set;
+
 @Repository
 public interface GenresRepository extends JpaRepository<Genres, Long>, JpaSpecificationExecutor {
     @Query(value = "UPDATE genres SET is_deleted = true, delete_date_time = now() WHERE id = :id", nativeQuery = true)
     public Genres deleteById(@Param("id") Long id);
+
+    public Set<Genres> findByIdIn(Set<Long> ids);
 }
