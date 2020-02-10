877538dda87526b14ba022cd32344e280e11f9c3
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-02-10 23:25:55 +0900

    ハードコードなどのリファクタを実施

--- src/main/java/source/infrastructure/scraping/images/IScrapingImage.java
+++ src/main/java/source/infrastructure/scraping/images/IScrapingImage.java
@@ -1,9 +1,8 @@
 package source.infrastructure.scraping.images;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 public interface IScrapingImage {
-    public List<String> pickImageAddresses() throws UnsupportedEncodingException, IOException;
+    public List<String> pickImageAddresses() throws IOException;
 }
--- src/main/java/source/infrastructure/scraping/images/StreetFashionSnap.java
+++ src/main/java/source/infrastructure/scraping/images/StreetFashionSnap.java
@@ -1,7 +1,6 @@
 package source.infrastructure.scraping.images;
 
 import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -9,8 +8,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
@@ -23,11 +22,9 @@ public class StreetFashionSnap implements IScrapingImage {
     private String streetFashionSnapUrl;
 
     @Override
-    public List<String> pickImageAddresses() throws UnsupportedEncodingException, IOException {
-        String accessURL = new StringBuilder()
-                .append(URLDecoder.decode(this.streetFashionSnapUrl, "UTF-8"))
-                .append(new Random().nextInt(100) + 1)
-                .toString();
+    public List<String> pickImageAddresses() throws IOException {
+        String accessURL = URLDecoder.decode(this.streetFashionSnapUrl, StandardCharsets.UTF_8.name()) +
+                (new Random().nextInt(100) + 1);
 
         Document document = Jsoup
                 .connect(accessURL)
