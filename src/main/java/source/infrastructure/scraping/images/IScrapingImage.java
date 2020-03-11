8c16491225123c94636c933642795ab5c658ea86
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-03-11 09:43:20 +0900

    スクレイピング処理を削除

--- src/main/java/source/infrastructure/scraping/images/IScrapingImage.java
+++
@@ -1,8 +0,0 @@
-package source.infrastructure.scraping.images;
-
-import java.io.IOException;
-import java.util.List;
-
-public interface IScrapingImage {
-    public List<String> pickImageAddresses() throws IOException;
-}
