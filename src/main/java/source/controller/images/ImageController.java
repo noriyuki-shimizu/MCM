1d8ab37b43db05ca8f1bffc21be1588a7b8f9d95
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-03-25 09:05:07 +0900

    不要な処理を削除

--- src/main/java/source/controller/images/ImageController.java
+++
@@ -1,9 +0,0 @@
-package source.controller.images;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(value = "/images")
-public class ImageController {
-}
--- src/main/java/source/usecases/app/images/IGetImageAddressUsecase.java
+++
@@ -1,7 +0,0 @@
-package source.usecases.app.images;
-
-import java.util.List;
-
-public interface IGetImageAddressUsecase {
-    List<String> handle();
-}
