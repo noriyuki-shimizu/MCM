fcc02f8de0d8129c201989a7c4d34414c682ed4e
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-03-29 15:21:30 +0900

    パスの設定を修正

--- src/main/java/source/controller/HealthController.java
+++ src/main/java/source/controller/HealthController.java
@@ -2,13 +2,15 @@ package source.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import source.annotation.NonAuth;
 
 @RestController
+@RequestMapping(value = "/health")
 public class HealthController {
-    @GetMapping("/health")
+    @GetMapping()
     @NonAuth
     @ResponseStatus(HttpStatus.OK)
     public String health() {
