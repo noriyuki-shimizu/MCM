a2e8649b6c20df9d0f8875a5d760e8133da5b77d
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-04-23 19:02:36 +0900

    お店の crud 処理に対しログ出力するよう修正

--- src/main/java/source/controller/shops/crud/ShopCrudController.java
+++ src/main/java/source/controller/shops/crud/ShopCrudController.java
@@ -1,6 +1,8 @@
 package source.controller.shops.crud;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import source.controller.shops.crud.request.ShopUpdateRequestModel;
 import source.controller.shops.crud.response.ShopResponseViewModel;
 import source.controller.shops.crud.response.ShopResponseViewModels;
 
+@Slf4j(topic = "source.controller.shops.crud")
 @RestController
 @RequiredArgsConstructor
 public class ShopCrudController extends ShopsController {
@@ -22,19 +25,22 @@ public class ShopCrudController extends ShopsController {
     public ShopResponseViewModel handleCreate(
             @PathVariable("userId") Long userId,
             @RequestBody ShopCreateRequestModel requestData
-    ) {
+    ) throws JsonProcessingException {
+        log.info("create -> user: {}, data: {}", userId, MAPPER.writeValueAsString(requestData));
         return this.usecase.create(userId, requestData);
     }
 
     @DeleteMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void handleDelete(@PathVariable("id") Long id) {
+    public void handleDelete(@PathVariable("userId") Long userId, @PathVariable("id") Long id) {
+        log.info("delete -> user: {}, id: {}", userId, id);
         this.usecase.delete(id);
     }
 
     @PutMapping(value = "/{id}/restoration")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void handleRestoration(@PathVariable("id") Long id) {
+    public void handleRestoration(@PathVariable("userId") Long userId, @PathVariable("id") Long id) {
+        log.info("restoration -> user: {}, id: {}", userId, id);
         this.usecase.restoration(id);
     }
 
@@ -56,7 +62,8 @@ public class ShopCrudController extends ShopsController {
             @PathVariable("userId") Long userId,
             @PathVariable("id") Long id,
             @RequestBody ShopUpdateRequestModel requestData
-    ) {
+    ) throws JsonProcessingException {
+        log.info("update -> user: {}, data: {}", userId, MAPPER.writeValueAsString(requestData));
         this.usecase.update(userId, id, requestData);
     }
 }
